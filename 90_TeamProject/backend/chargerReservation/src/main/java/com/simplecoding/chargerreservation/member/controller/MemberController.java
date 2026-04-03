009046532cfd87c998d3cf7bcd19bd3dd38d47da
bakeunbi99 <peb7949@naver.com>
2026-04-03 14:48:38 +0900

    [추가] 회원 인증 API 구현
    [추가] 회원 인증 API 구현

---
+++ 90_TeamProject/backend/chargerReservation/src/main/java/com/simplecoding/chargerreservation/member/controller/MemberController.java
@@ -0,0 +1,65 @@
+package com.simplecoding.chargerreservation.member.controller;
+
+import com.simplecoding.chargerreservation.member.dto.MemberDto;
+import com.simplecoding.chargerreservation.member.dto.TokenDto;
+import com.simplecoding.chargerreservation.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+    private final MemberService memberService;
+
+    // 회원가입
+    @PostMapping("/join")
+    public ResponseEntity<String> join(@RequestBody MemberDto memberDto) {
+        memberService.join(memberDto);
+        return ResponseEntity.ok("회원가입이 완료되었습니다.");
+    }
+
+    // 로그인 (성공 시 AT, RT 발급)
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody MemberDto memberDto) {
+        try{
+            // 실제 운영 시에는 HttpServletRequest에서 IP와 기기 정보를 추출하지만, 우선 임시값으로 테스트합니다.
+            TokenDto tokenDto = memberService.login(memberDto.getLoginId(), memberDto.getLoginPw(), "Web-Browser", "127.0.0.1");
+            log.info("로그인 성공: {}", memberDto.getLoginId());
+            return ResponseEntity.ok(tokenDto);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
+    }
+
+    // 토큰 재발급 (AccessToken 만료 시 리액트에서 호출)
+    @PostMapping("/refresh")
+    public ResponseEntity<TokenDto> refresh(@RequestBody String refreshToken) {
+        // 클라이언트가 보낸 RT로 새 AT를 발급
+        TokenDto newAccessToken = memberService.refreshAccessToken(refreshToken);
+        return ResponseEntity.ok(newAccessToken);
+    }
+
+    // 로그아웃 (DB에서 RT 삭제)
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(Authentication authentication) {
+        // 토큰이 유효하다면 SecurityContextHolder에서 인증 정보 자동 주입
+        if (authentication == null) {
+            return ResponseEntity.status(401).body("인증 정보가 없습니다.");
+        }
+
+        String loginId = authentication.getName();      // 현재 로그인한 유저의 정보를 가져옴
+        memberService.logout(loginId);
+
+        return ResponseEntity.ok("로그아웃 되었습니다.");
+    }
+
+}
--- 90_TeamProject/backend/chargerReservation/src/main/java/com/simplecoding/chargerreservation/member/repository/MemberRepository.java
+++ 90_TeamProject/backend/chargerReservation/src/main/java/com/simplecoding/chargerreservation/member/repository/MemberRepository.java
@@ -8,11 +8,11 @@ import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    // 1. 로그인 시 아이디로 회원 정보를 찾기 위해 필요
+    // 로그인 시 아이디로 회원 정보를 찾기 위해 필요
     Optional<Member> findByLoginId(String loginId);
     // 소셜 로그인 중복 확인 및 기존 회원 찾기
     Optional<Member> findByProviderAndProviderId(String provider, String providerId);
 
-    // 2. 회원가입 시 아이디 중복 체크를 위해 필요
+    // 회원가입 시 아이디 중복 체크를 위해 필요
     boolean existsByLoginId(String loginId);
 }
