2469558ca14f0b790549f3e7094ad01b2f25ebb8
bakeunbi99 <peb7949@naver.com>
2026-04-06 16:07:09 +0900

    [수정] JPA Auditing 설정 추가
    [수정] JPA Auditing 설정 추가

--- 90_TeamProject/backend/chargerReservation/src/main/java/com/simplecoding/chargerreservation/ChargerReservationApplication.java
+++ 90_TeamProject/backend/chargerReservation/src/main/java/com/simplecoding/chargerreservation/ChargerReservationApplication.java
@@ -2,7 +2,9 @@ package com.simplecoding.chargerreservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ChargerReservationApplication {
 
